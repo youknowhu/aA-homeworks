16381a37da1a97a21a9700b90cd103781649072e
youknowhu <kimberly.hu@gmail.com>
2018-03-27 20:50:49 -0700

    Create Giphys App

---
+++ W7D3/giphys/giphy_search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import { fetchSearchGiphys } from './actions/giphy_actions';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+});
